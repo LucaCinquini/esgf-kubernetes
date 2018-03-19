790e896daff65d6362cdba620f8053682836e1fb
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-03-19 12:14:23 -0600

    Removing the proxy on cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script to delete all ESGF Kubernetes objects
 
-kubectl delete deployment,svc,statefulset -l stack=esgf
+kubectl delete deployment,svc,statefulset,ingress -l stack=esgf
 kubectl delete secrets esgf-auth-secrets esgf-cog-secrets esgf-hostcert esgf-postgres-secrets esgf-slcs-ca esgf-tds-secrets esgf-slcs-secrets
 kubectl delete configmap esgf-config esgf-trust-bundle

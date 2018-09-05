d859e39487c9fba001dcf8dea53a03c7413db379
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-09-05 09:37:45 -0600

    Updating cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,4 +5,4 @@ kubectl delete deployment,svc,statefulset,ingress -l stack=esgf
 kubectl delete secrets esgf-auth-secrets esgf-cog-secrets esgf-hostcert esgf-postgres-esgcet-secrets esgf-slcs-ca esgf-tds-secrets esgf-slcs-secrets esgf-publisher-secrets
 kubectl delete configmap esgf-config esgf-trust-bundle
 kubectl delete pvc -l stack=esgf
-kubectl delete pv --all
+#kubectl delete pv --all
